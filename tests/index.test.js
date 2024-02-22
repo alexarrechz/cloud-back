d03d4bd56c4fea79e363eb3236fd64a6dd0a021a
alexarrechz <ivanalejandroarreolachavez@gmail.com>
2024-02-21 20:56:37 -0600

    added test into test ahora bien andale

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,7 +2,7 @@ describe('Sample Test', () => {
     test('should test that true === true', () => {
         expect(true).toBe(true)
     })
-    test('should test that true === true', () => {
+    test('should test that false === false', () => {
         expect(false).toBe(false)
         // jejejejejej
     })
