c202cda797f2fb25f85d6241bd5b5537877cd8c6
eliangerard <eliangerardgg@gmail.com>
2024-03-18 17:05:13 -0600

    Added user routes and mongo connection

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
+const cors = require('cors')
+const user = require('./routes/user');
 
+mongoose.connect(process.env.DATABASE_URL)
+
+mongoose.connection.on('error', (error) => console.error(error))
+mongoose.connection.once('open', () => console.log('Connected to Database'))
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/users', user);
 
 app.get('/', (req, res) => {
     res.send('¡Hola Mundo!');
@@ -10,3 +22,4 @@ app.get('/', (req, res) => {
 app.listen(process.env.PORT, () => {
     console.log(`El servidor está escuchando en el puerto ${process.env.PORT}`);
 });
+
