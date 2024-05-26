ea6b28fa916aab8890628ed25a75feb892b5f324
eliangerard <eliangerardgg@gmail.com>
2024-05-26 12:27:46 -0600

    Add stripe and subscription routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const verifyToken = require('../middlewares/verifyToken');
-const { getUser, getUsersByID, getAllUsers, addUser, subscribe } = require('../controllers/userController');
+const { getUser, getUsersByID, getAllUsers, addUser, subscribe, subscribed, stripeAccountLink, stripeAccount, stripeProducts, checkout } = require('../controllers/userController');
 
-router.post('/', addUser );
-
-router.get('/all', getAllUsers );
-
-router.get('/:id', getUsersByID );
-
-router.get('/', verifyToken, getUser);
+router.post('/', addUser);
+router.get('/all', getAllUsers);
+router.get('/subscribed', verifyToken, subscribed);
 
 router.post('/subscribe', verifyToken, subscribe);
+
+router.get('/:id', getUsersByID);
+router.get('/', verifyToken, getUser);
+
+router.post('/account', verifyToken, stripeAccount);
+router.post('/account/link', verifyToken, stripeAccountLink);
+
+router.get('/account/products', verifyToken, stripeProducts)
+router.post('/checkout', verifyToken, checkout)
+
+
 module.exports = router;
