3784fa2d8ab6f6a7410c3719e74e28726b5e0a98
eliangerard <eliangerardgg@gmail.com>
2024-05-27 23:40:36 -0600

    Add only admin users authorization

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const verifyToken = require('../middlewares/verifyToken');
-const { getUser, getUsersByID, getAllUsers, addUser, subscribe, subscribed, stripeAccountLink, stripeAccount, stripeProducts, stripeProduct, checkout, updateUser, getSettings, updateSettings } = require('../controllers/userController');
+const { getUser, getUsersByID, getAllUsers, addUser, subscribe, subscribed, stripeAccountLink, stripeAccount, stripeProducts, stripeProduct, checkout, updateUser, getSettings, updateSettings, toggleAdmin } = require('../controllers/userController');
 
 router.post('/', addUser);
 router.patch('/', verifyToken, updateUser);
-router.get('/all', getAllUsers);
+router.get('/all', verifyToken, getAllUsers);
+router.patch('/admin/:id', verifyToken, toggleAdmin);
 router.get('/settings', verifyToken, getSettings);
 router.patch('/settings', verifyToken, updateSettings);
 router.get('/subscribed', verifyToken, subscribed);
